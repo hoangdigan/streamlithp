661c47675e5df5df1f5ed095c5ad518d1cf407ac
Hoang Pham <hoangdigan@gmail.com>
2022-06-22 14:37:35 +0700

    first commit

---
+++ app.py
@@ -0,0 +1,8 @@
+import streamlit as st
+
+st.title('Our First Streamlit App')
+st.subheader('Introducing Streamlit inAutomate Everyting with Python')
+st.write("""
+    This is our first web app.
+    Enyoyed it!
+""")
