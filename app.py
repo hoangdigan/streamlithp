2deed9429b582fea431ad61cfb43e3f01f3c4ddc
Hoang Pham <hoangdigan@gmail.com>
2022-06-22 19:19:51 +0700

    2nd

--- app.py
+++ app.py
@@ -1,4 +1,13 @@
 import streamlit as st
+import pandas
+
+
+data =[
+    'series1': [1,3,4,5,7],
+    'series2': [10.30.40.60.70]
+]
+
+df=pandas.DataFrame(data)
 
 st.title('Our First Streamlit App')
 st.subheader('Introducing Streamlit inAutomate Everyting with Python')
@@ -6,3 +15,5 @@ st.write("""
     This is our first web app.
     Enyoyed it!
 """)
+
+st.write(df)
